9916627f07f66a56502946dd12b3c2acf4cb0d72
Jeremy Tsang <jeremytsang1@gmail.com>
2020-08-22 21:24:37 -0700

    Move secret key to environment variable

--- listacious/listacious/settings.py
+++ listacious/listacious/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'h_u^%gi=t%_jj*y%hk1w_o1ao+=t&auiat37%hvo^xj0+!wt+^'
+SECRET_KEY = os.environ.get('LISTACIOUS_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
